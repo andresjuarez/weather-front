b2da80de1c7ae23a3a51a8b2776138e10453f5db
andresalejandro.juarez@externos-cl.cencosud.com <andresalejandro.juarez@externos-cl.cencosud.com>
2019-07-11 17:14:49 -0400

    Adding src app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import socketIOClient from 'socket.io-client'
+import moment from 'moment'
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  state = {
+    endpoint: 'http://wheaterbeanstalk-env-1.us-east-1.elasticbeanstalk.com',
+    // endpoint: 'http://localhost',
+    ts: moment().format('LTS'),
+    data: {}
+  }
+
+  componentDidMount () {
+    const socket = socketIOClient(this.state.endpoint)
+    socket.on('WEATHER', data => this.setState({data: Object.assign({}, data, this.state.data), ts: new Date()}, () => console.log('updated')))
+  }
+
+  _getTs = (ts) => `${ts}`
+
+  _getInfoBox = ({key, value: {temperature, datetime}}) => {
+    const tempNumber = parseInt(temperature)
+    return (
+      <div key={key} style={{ height: 150, width: 150, backgroundColor: 'blue', color: 'white', margin: '0px 10px', display: 'flex', flexDirection: 'column' }}>
+        <p>{key}</p>
+        <p>{Math.round((tempNumber - 32) * 5/9) } ºC</p>
+        <span>{moment(datetime.substr(0, datetime.length-6)).format('lll')}</span>
+      </div>
+    )
+  }
+
+  _sortElements = (a,b) => {
+      const keyA= a.key.toLowerCase(), keyB=b.key.toLowerCase()
+      if (keyA < keyB) return -1
+      if (keyA > keyB) return 1
+      return 0
+  }
+
+
+  render () {
+    const { ts, data } = this.state
+    console.log('DEBUG: render -> data', data)
+    return (
+      <div className='App'>
+        <header>
+          <h4>
+          {this._getTs(ts)}
+          </h4>
+        </header>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <div  style={{ display: 'flex' }}>
+            {Object.entries(data).map(([key, value]) => ({ key, value })).sort(this._sortElements).map(this._getInfoBox)}
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
